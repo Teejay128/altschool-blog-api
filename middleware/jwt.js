1b03ba93c63d2d863e4362d4403c2011b7d0b3b8
teejay128 <104542152+Teejay128@users.noreply.github.com>
2022-11-04 11:10:26 +0100

    merging jwt into master branck

--- middleware/jwt.js
+++ middleware/jwt.js
@@ -2,5 +2,5 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = createToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET || "secret", { expiresIn: 3 * 24 * 60 * 60 });
+    return jwt.sign(id, process.env.JWT_SECRET || "secret", { expiresIn: 3 * 24 * 60 * 60 });
 }
