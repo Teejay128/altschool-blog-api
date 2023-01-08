7011ec0de901dcf60b6f85056e73d22ab6603b16
teejay128 <104542152+Teejay128@users.noreply.github.com>
2023-01-08 23:55:27 +0100

    Added rate limiter and helmet for security

--- src/app.js
+++ src/app.js
@@ -1,32 +1,45 @@
 const express = require('express');
 const morgan = require('morgan')
 const path = require('path')
+const helmet = require('helmet')
+const rateLimit = require("express-rate-limit");
 
 const cookieParser = require('cookie-parser');
 const userRouter = require('./routes/userRoutes')
-const articleRouter = require('./routes/articleRoutes')
+const articleRouter = require('./routes/articleRoutes');
+const { each } = require('lodash');
 
 const app = express();
 
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(cookieParser());
 app.use(morgan("tiny"));
 
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'))
+app.set('trust proxy', 1) // Trust the first proxy
+
+app.use(
+    rateLimit({
+        windowMs: 60 * 60 * 1000, // 1 minute
+        max: 50, // 50 requests per minute
+        standardHeaders: true,
+        legacyHeaders: false
+    })
+)
+
+app.use(helmet())
 
 // Routes
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/blog', articleRouter);
 
 app.get('/', (req, res) => {
-    res.redirect('/api/v1/blog')
-    // return res.status(200).json({
-    //     status: "success",
-    //     message: "Welcome to technobyte"
-    // });
+    return res.status(200).json({
+        status: "success",
+        message: "This is my altschool blogging api project"
+    })
 });
 
 // 404 page
